23d56a5dc7cbd2f709bfb0a371eb2dd7357008e6
Codetector <codetector@codetector.org>
2022-05-06 00:30:36 -0400

    queue iterator

--- kernel/include/util/queue.h
+++ kernel/include/util/queue.h
@@ -2,6 +2,11 @@
 
 #include "types.h"
 
+typedef struct dequeue_iterator {
+  struct dequeue *q;
+  struct dequeue_node* current_node;
+} dequeue_iterator_t;
+
 typedef struct dequeue {
   size_t size;
   struct dequeue_node* head;
@@ -14,9 +19,14 @@ struct dequeue_node {
   struct dequeue_node* prev;
 };
 
-void dequeue_construct(dequeue_t *q);
-void dequeue_push_back(dequeue_t *q, void* item);
-void dequeue_push_front(dequeue_t *q, void* item);
+void dequeue_construct(dequeue_t* q);
+void dequeue_push_back(dequeue_t* q, void* item);
+void dequeue_push_front(dequeue_t* q, void* item);
 void* dequeue_peek_front(dequeue_t* q);
 void* dequeue_pop_front(dequeue_t* q);
 void* dequeue_pop_back(dequeue_t* q);
+
+void dequeue_iterator(dequeue_t* q, dequeue_iterator_t* iter);
+void* dequeue_iterator_next(dequeue_iterator_t* iter);
+bool dequeue_iterator_has_next(dequeue_iterator_t* iter);
+void dequeue_iterator_delete(dequeue_iterator_t* iter);
--- kernel/src/util/queue.c
+++ kernel/src/util/queue.c
@@ -69,3 +69,55 @@ void* dequeue_pop_back(dequeue_t* q) {
   panic("not implemented");
   return NULL;
 }
+
+void dequeue_iterator(dequeue_t* q, dequeue_iterator_t* iter) {
+  iter->current_node = q->head;
+  iter->q = q;
+}
+
+void* dequeue_iterator_next(dequeue_iterator_t* iter) {
+  void* data = NULL;
+  if (iter->current_node != NULL) {
+    data = iter->current_node->data;
+    iter->current_node = iter->current_node->next;
+  }
+  return data;
+}
+
+bool dequeue_iterator_has_next(dequeue_iterator_t* iter) {
+  return iter->current_node != NULL;
+}
+
+void dequeue_iterator_delete(dequeue_iterator_t* iter) {
+  if (iter->q->size == 0) {
+    return;
+  }
+  if (iter->q->size == 1) {
+    (void)dequeue_pop_front(iter->q);
+    return;
+  }
+  struct dequeue_node* target_node = NULL;
+  if (iter->current_node == NULL) {
+    target_node = iter->q->tail;
+  } else {
+    target_node = iter->current_node->prev;
+  }
+  if (target_node != NULL) {
+    // Not head
+    if (target_node->prev != NULL) {
+      target_node->prev->next = target_node->next;
+    } else {
+      iter->q->head = target_node->next;
+    }
+
+    // Not Tail
+    if (target_node->next != NULL) {
+      target_node->next->prev = target_node->prev;
+    } else {
+      iter->q->tail = target_node->prev;
+    }
+    target_node->next = NULL;
+    target_node->prev = NULL;
+    iter->q->size--;
+  }
+}
