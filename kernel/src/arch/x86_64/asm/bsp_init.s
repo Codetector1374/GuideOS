323b785fa4ca5ab3c3a6d4ab43bb156754f03006
Codetector <codetector@codetector.org>
2022-05-14 19:41:27 -0400

    [Doc] bsp_init.s documentation update

--- kernel/src/arch/x86_64/asm/bsp_init.s
+++ kernel/src/arch/x86_64/asm/bsp_init.s
@@ -20,29 +20,33 @@ kentry:
     push ebx
     push eax
 
+# Enable CR4.PAE bit
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+# Set IA32_EFER.LME (EFER MSR, Long Mode Enable)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
 # Build Page Tables
     mov eax, OFFSET pdp
     or eax, 0b11
     mov [pml4], eax
     mov [pml4 + (256 * 8)], eax
 
-# Enable Paging / Long Mode
+# Load CR3 (PTBR) with PML4 physical address
     mov eax, OFFSET pml4
     mov cr3, eax
 
-    mov eax, cr4
-    or eax, 1 << 5
-    mov cr4, eax
-
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1 << 8
-    wrmsr
-
+# Set CR0.PG (Paging Enable)
     mov eax, cr0
     or eax, 1 << 31
     mov cr0, eax
 
+# Load new GDT and long jump to update CS (code segment selector)
     lgdt    [gdtdesc]
     jmp     0x8:long_start
 
